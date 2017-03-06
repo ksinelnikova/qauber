64478ed0144750a0cfe255ee4b5f7ca612bf9516
ksinelnikova <kristina.siusa@gmail.com>
2017-03-05 22:52:57 -0800

    Homework of Lesson 7 of Level 1

---
+++ level1_homework7/src/main/java/com/example/tests/class7_assignment_kristina_sinelnikova.java
@@ -0,0 +1,32 @@
+package project;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class class7_assignment_kristina_sinelnikova {
+
+	public static void main(String[] args) throws InterruptedException {
+		FirefoxDriver wb = new FirefoxDriver();
+		wb.manage().timeouts().implicitlyWait(7, TimeUnit.SECONDS);
+		
+		
+		wb.get("http://testwave.qabidder.net/#/page/register-sa");
+		String title = wb.getTitle();
+		System.out.println(title);
+		String url = wb.getCurrentUrl();
+		System.out.println(url);
+		WebElement inputName = wb.findElement(By.xpath("html/body/div[2]/div/div/div[1]/div/div[2]/form/div[1]/input"));
+		inputName.sendKeys("tester");
+		WebElement inputEmail = wb.findElement(By.xpath("html/body/div[2]/div/div/div[1]/div/div[2]/form/div[2]/input"));
+		inputEmail.sendKeys("ubertesterqa@gmail.com");
+		WebElement inputPassword = wb.findElement(By.xpath(".//*[@id='id-password']"));
+		inputPassword.sendKeys("12345password");
+		WebElement inputPasswordAgain = wb.findElement(By.xpath("html/body/div[2]/div/div/div[1]/div/div[2]/form/div[4]/input"));
+		inputPasswordAgain.sendKeys("12345password");
+		Thread.sleep(5000);
+		WebElement register = wb.findElement(By.xpath("html/body/div[2]/div/div/div[1]/div/div[2]/form/button"));
+		register.click();
+	}
+}
