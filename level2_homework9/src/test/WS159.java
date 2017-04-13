03920170753a4c9377b8086aaa6ecc3f8ce77ece
ksinelnikova <kristina.siusa@gmail.com>
2017-04-12 23:22:56 -0700

    Homework of Lesson 9 of Level 2

---
+++ level2_homework9/src/test/WS159.java
@@ -0,0 +1,37 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WS159 {
+	public static void main(String[] args) throws InterruptedException {
+	// Create WebDriver Object 
+	System.setProperty("webdriver.chrome.driver", "/Users/kristina_sinelnikova/Downloads/chromedriver");
+	ChromeDriver driver = new ChromeDriver(); driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+	driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+	//Go to Login Page of TestWave
+	driver.get("http://testwave.qabidder.net/#/page/login");
+	
+	//Login into TestWave
+	WebElement usernameTextBox = driver.findElement(By.id("exampleInputEmail1"));
+	 usernameTextBox.sendKeys("ubertesterqa@gmail.com");
+	WebElement passwordTextBox = driver.findElement(By.id("exampleInputPassword1"));
+	passwordTextBox.sendKeys("12345password");
+	
+	Thread.sleep(3000);
+	WebElement loginButton = driver.findElement(By.tagName("button")); loginButton.click();
+	
+	//Uncheck Published On filter
+	WebElement enablePublishedOn = driver.findElement(By.cssSelector("input[ng-model='enablePublishedOn']"));
+	enablePublishedOn.click();
+	
+	//Input last name
+	WebElement LastNameInput = driver.findElement(By.cssSelector("input[ng-model='slnSearchKey']"));
+	LastNameInput.sendKeys("Smith");
+	
+	}
+}
+	
---
+++ level2_homework9/src/test/WS162.java
@@ -0,0 +1,42 @@
+package test;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WS162 {
+	public static void main(String[] args) throws InterruptedException {
+	// Create WebDriver Object 
+	System.setProperty("webdriver.chrome.driver", "/Users/kristina_sinelnikova/Downloads/chromedriver");
+	ChromeDriver driver = new ChromeDriver(); driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+	//Go to Login Page of TestWave
+	driver.get("http://testwave.qabidder.net/#/page/login");
+	
+	//Login into TestWave
+	WebElement usernameTextBox = driver.findElement(By.id("exampleInputEmail1"));
+	 usernameTextBox.sendKeys("ubertesterqa@gmail.com");
+	WebElement passwordTextBox = driver.findElement(By.id("exampleInputPassword1"));
+	passwordTextBox.sendKeys("12345password");
+	
+	Thread.sleep(3000);
+	WebElement loginButton = driver.findElement(By.tagName("button")); loginButton.click();
+	
+	//Uncheck Published On filter
+	WebElement enablePublishedOn = driver.findElement(By.cssSelector("input[ng-model='enablePublishedOn']"));
+	enablePublishedOn.click();
+	
+	//Input last name
+	WebElement LastNameInput = driver.findElement(By.cssSelector("input[ng-model='slnSearchKey']"));
+	LastNameInput.sendKeys("Ramirez");
+	
+	Thread.sleep(3000);
+	WebElement SearchResult = driver.findElement(By.cssSelector("tr[ng-show='!filteredReports.length']"));
+	if(SearchResult.getText().equals("0 results found.")){
+		System.out.print("Test case passed");
+	}else{
+		System.out.println("Test case failed");
+	}
+}}
+	
